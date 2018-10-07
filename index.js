9a8a9220eabb404a6a0658d412d914581f1075fe
Yadiel <ycabr035@fiu.edu>
2018-10-06 21:42:03 -0400

    added routes

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ app.get('/api/courses', (req, res) => {
   res.send([1,2,3]);
 });
 
-app.get('/api/posts/:year/:month', (req, res) => {
-  res.send(req.params);
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found');
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
