e6e72961a413d9ea914a2fabda5f4fde0467b5f9
Yadiel <ycabr035@fiu.edu>
2018-10-06 21:50:33 -0400

    added routes

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 
+//courses array 
+const courses = [
+   {id: 1, name: 'course1'},
+   {id: 2, name: 'course2'},
+   {id: 3, name: 'course3'}
+
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1,2,3]);
+  res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
@@ -16,4 +24,4 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Listening on port ${port}...'));
+app.listen(port, () => console.log('Listening on port ${port} ...'));
