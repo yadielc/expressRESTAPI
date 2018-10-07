5821e56a3d308e856d8c544c6b2c6aa44d469801
Yadiel <ycabr035@fiu.edu>
2018-10-06 22:19:47 -0400

    added routes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Joi = require('joi'); 
+const Joi = require('joi');
 const app = express();
 
 // Use middleware
@@ -29,6 +29,13 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+  const schema = {
+       name: Joi.string().min(3).required()
+
+  };
+
+  const result = Joi.validate(req.body, schema);
+  console.log(result); 
   if(!req.body.name || req.body.name.length < 3){
     //400 Bad Request
     res.status(400).send('Name is required and should be minimum 3 characters');
