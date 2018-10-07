cf3cfd6ae38b48debbe56f77bab27c0e59aef197
Yadiel <ycabr035@fiu.edu>
2018-10-06 21:35:24 -0400

    added routes

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.get('/api/courses', (req, res) => {
   res.send([1,2,3]);
 });
 
-app.get('/api/courses/:id', (req, res) => {
+app.get('/api/posts/:year/:month', (req, res) => {
   res.send(req.params.id);
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Listening on port ${port}...')); 
+app.listen(port, () => console.log('Listening on port ${port}...'));
